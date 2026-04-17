11f0163df99f8a387628b16472b9d46eb35826ab
cjy2103 <43211800+cjy2103@users.noreply.github.com>
2026-04-17 13:23:26 +0900

    Use AppCompat base theme to prevent missing Material3 style linking

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2025.02.00")
 
     implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
     implementation("androidx.activity:activity-compose:1.10.1")
@@ -64,7 +65,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-compose:2.8.6")
-    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
 
     implementation("androidx.room:room-runtime:2.6.1")
